1491d142eecfc0f95768852aa17225ccf3e3b0f6
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-02-14 01:28:23 +0300

    Task3

---
+++ Task3.cs
@@ -0,0 +1,12 @@
+﻿void parity (int a)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine($"Число {a} чётное");
+    }
+    else
+    {
+        Console.WriteLine($"Число {a} не чётное");
+    }
+}
+parity(-3);
